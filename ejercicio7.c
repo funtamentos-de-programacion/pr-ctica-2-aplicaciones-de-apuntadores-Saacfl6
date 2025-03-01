2a8b44d36e64d615d3ecba7f55f6b1b8297fcc85
ISAAC FLORES <isaacreyflo@gmail.com>
2025-03-01 00:48:46 -0600

    Update ejercicio7.c

--- ejercicio7.c
+++ ejercicio7.c
@@ -4,5 +4,8 @@
 int main() {
     int arr[5] = {10, 20, 30, 40, 50};
     // Código para imprimir direcciones de memoria aquí
+    for (int i = 0; i < 5; i++){
+        printf("arr[%d]: %p\n", i, &arr[i]);
+        }
     return 0;
 }
