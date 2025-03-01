2144d7ecc71b717541da300ae206414ce9c04bde
ISAAC FLORES <isaacreyflo@gmail.com>
2025-03-01 00:45:58 -0600

    Update ejercicio9.c

--- ejercicio9.c
+++ ejercicio9.c
@@ -3,11 +3,21 @@
 
 int sumaArreglo(int *arr, int tamaño) {
     // Implementar la suma aquí
-    return 0;
+    int suma = 0;
+    for (int i = 0; i < tamaño; i++)
+        suma += arr[i];
+    return suma;
 }
 
 int main() {
     int numeros[] = {1, 2, 3, 4, 5};
     // Código para calcular y mostrar la suma
+    int sumanum;
+    for (int i = 0; i < 5; i++) {
+        printf("numero[%d]: %d\n", i, numeros[i]);
+    }
+    printf("\n");
+    sumanum=sumaArreglo(numeros, 5);
+    printf("Suma de los numeros del arreglo = %d", sumanum);
     return 0;
 }
